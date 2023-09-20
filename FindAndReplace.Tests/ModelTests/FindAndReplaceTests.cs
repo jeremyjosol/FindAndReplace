c57cc7ae883b8284db6ff3e6f2c5de2a76872566
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-20 09:24:16 -0700

    Add boilerplate code
    Co-authored-by: Ian Bravo <bravo.ian@gmail.com>
    Co-authored-by: Onur Kaymak <onurkaymak@outlook.com>

---
+++ FindAndReplace.Tests/ModelTests/FindAndReplaceTests.cs
@@ -0,0 +1,13 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using FindAndReplace.Models;
+
+
+namespace FindAndReplace.Tests
+{
+  [TestClass]
+  public class FindAndReplaceTests
+  {
+    [TestMethod]
+    // Initialize first test
+  }
+}
---
+++ FindAndReplace/Program.cs
@@ -0,0 +1,13 @@
+using System;
+using FindAndReplace.Models;
+
+namespace FindAndReplace
+{
+  class Program
+  {
+    static void Main()
+    {
+
+    }
+  }
+}
