c52f842d049d0e97d43f510e6c0feaa624b82e9b
Jeremy Josol <jeremyjosol@gmail.com>
2023-09-20 22:17:21 -0700

    Create custom ASCII banner for UI

--- FindAndReplace/Program.cs
+++ FindAndReplace/Program.cs
@@ -7,41 +7,53 @@ namespace FindAndReplace
   {
     static void Main()
     {
+
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");      
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");      
+      Console.WriteLine(@"   ____  __  _    __  ____      _    __    ____    _____   ____     __     ____       _____   _____ ");
+      Console.WriteLine(@"  / __/ / / / \  / / / -- \    / \  / /   /    \  /  __/  /  -  \  / /    / /\ \     / ___/  /  __/ ");
+      Console.WriteLine(@" / __/ / / / / \/ / / /_/ /   / / \/ /   / /_/ / /  ---  /  /---` / /__  / /__\ \   / /__   /  --- ");
+      Console.WriteLine(@"/_/   /_/ /_/  \_/ /_____/   /_/  \_/   /_/ \_\ /____/  /__/     /____/ /_/    \_\ /____/  /____/ ");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");          
       Console.WriteLine("                                         ");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("Welcome to our FindAndReplace application.");
+      Console.WriteLine("Welcome to the FindAndReplace application.");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("                                         ");
-      Console.WriteLine("This application will take 3 inputs from you.");
+      Console.WriteLine("To get started, you will enter 3 inputs.");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("                                         ");
-      Console.WriteLine("1. A sentence of your choice:");
-      Console.WriteLine("2. A word you want replaced:");
-      Console.WriteLine("3. A word to replace with:");
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("1. A sentence of your choice:~*~*~*~*~*~*");
+      Console.WriteLine("2. A word you want replaced:*~*~*~*~*~*~*");
+      Console.WriteLine("3. A word to replace it with:~*~*~*~*~*~*");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
       Console.WriteLine("                  ");
       Console.WriteLine("Let's get started!");
-      Console.WriteLine("Enter an interesting sentence:");
+      Console.WriteLine(" ");
+      Console.WriteLine("Enter a sentence:");
       string userInput = Console.ReadLine();
-      Console.WriteLine("Enter a word in that sentence you want to replace:");
+      Console.WriteLine(" ");
+      Console.WriteLine("Enter a word in the sentence you want to replace:");
       string findWord = Console.ReadLine();
-      Console.WriteLine("Enter a word you want to replace the previous word with:");
+      Console.WriteLine(" ");
+      Console.WriteLine("Enter a word you want to replace that word with:");
       string replacedWith = Console.ReadLine();
-      Console.WriteLine("Generating...");
-      Console.WriteLine("..............");
-      Console.WriteLine("...............");
-      Console.WriteLine("................");
-      Console.WriteLine("                  ");
+      Console.WriteLine(" ");
+      Console.WriteLine(" ");
       string result = FindAndReplaceWord.WordReplacer(userInput, findWord, replacedWith);
+      Console.WriteLine("Generating result...");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
+      Console.WriteLine(" ");
       Console.WriteLine($"Result: {result}");
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*~*");
-      Console.WriteLine("Please enter 'new' if you would like to generate a new sentence:");
+      Console.WriteLine(" ");
+      Console.WriteLine("Please enter 'new' if you would like to generate a new sentence.");
+      Console.WriteLine("Otherwise, enter any key to exit the application.");
+      Console.WriteLine(" ");
       string userResponse = Console.ReadLine();
       if (userResponse == "new" || userResponse == "New")
       {
